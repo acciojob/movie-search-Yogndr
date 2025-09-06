405873ba34294d45bdc76a31615cc1070c584d7a
Yogender <yashmittal92002@gmail.com>
2025-09-06 14:20:53 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,57 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+
+
 const App = () => {
+  const[movie,setMovie]=useState("");
+  const[fullinfo,setFullInfo]=useState([]);
+  const[error,setError]=useState("")
+
+  
+   const fetchmovies=async()=>{
+    try{
+      const response=await fetch(`https://www.omdbapi.com/?s=${encodeURIComponent(movie)}&apikey=99eb9fd1`)
+      const ans=await response.json();
+
+      if(ans.Response==="True"){
+      setFullInfo(ans.Search);
+    setError("");}
+    else{
+      setFullInfo([]);
+      setError("Invalid movie name. Please try again.")
+    }
+
+    }
+   catch (err) {
+      console.error("Fetch error:", err);
+      setFullInfo([]);
+      setError("Something went wrong. Please try again.");
+    }
+    
+
+   }
   return (
     <div>
-        {/* Do not remove the main div */}
+       Search
+       <br/>
+       <input type="text" value={movie} onChange={(e)=>{
+        setMovie(e.target.value)
+       }}/>
+       <button type="submit" onClick={fetchmovies} >Search</button>
+       {error && <p className="error">{error}</p>}
+       <ul>
+        {fullinfo.map((info)=>(
+          <li key={info.imdbID}>
+            <h4>{info.Title} {info.Year}</h4>
+            <img src={info.Poster!=="N/A"?info.Poster:"https://via.placeholder.com/100"}
+            alt={info.Title}
+            width="100"/>
+          </li>
+
+        ))}
+       </ul>
     </div>
   )
 }
